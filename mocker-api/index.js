cd8da9575d428158a8df8e966372106115eee93b
louis@trin-tech.net <vietanh.kvh@gmail.com>
2024-05-16 17:51:21 +0700

    feat: config deploy

--- mocker-api/index.js
+++ mocker-api/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const domain = process.env.APP_API_URL
-const port = process.env.PORT
+const port = process.env.PORT || 3001;
 // Example API endpoint
 app.get('/api/hello', async (req, res) => {
     res.json({ message: 'Hello from Mocker API!' });
 });
 
 app.listen(port, () => {
-    console.log(`Mocker API running at ${domain}`);
+    console.log(`Mocker API running at http://localhost:${port}`);
 });
