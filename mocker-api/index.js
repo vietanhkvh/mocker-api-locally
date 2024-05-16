e5a868e2ae6152359f968bf9c2b0d31778b57bd2
louis@trin-tech.net <vietanh.kvh@gmail.com>
2024-05-16 17:25:10 +0700

    feat: add UI

--- mocker-api/index.js
+++ mocker-api/index.js
@@ -1,13 +1,12 @@
-// my-app/mocker-api/index.js
 const express = require('express');
 const app = express();
-const port = 3001;
-
+const domain = process.env.APP_API_URL
+const port = process.env.PORT
 // Example API endpoint
-app.get('/api/hello', (req, res) => {
+app.get('/api/hello', async (req, res) => {
     res.json({ message: 'Hello from Mocker API!' });
 });
 
 app.listen(port, () => {
-    console.log(`Mocker API running at http://localhost:${port}`);
+    console.log(`Mocker API running at ${domain}`);
 });
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,107 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { endPointPost } from './constants';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
-const App = () => {
-  const [posts, setPosts] = useState([]);
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [editId, setEditId] = useState(null);
-
-
-  const fetchPosts = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/${endPointPost}`)
-      .then(response => response.json())
-      .then(data => setPosts(data));
-  };
+function App() {
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetchPosts()
+    fetch(`${process.env.REACT_APP_API_URL}/api/hello`)
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
   }, []);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const post = { title, content };
-
-    if (editId) {
-      fetch(`http://localhost:5000/posts/${editId}`, {
-        method: 'PUT', // or 'PATCH'
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(post)
-      })
-        .then(response => response.json())
-        .then(data => {
-          setPosts(posts.map(p => (p.id === editId ? data : p)));
-          setTitle('');
-          setContent('');
-          setEditId(null);
-        });
-    } else {
-      fetch(`${process.env.REACT_APP_API_URL}/${endPointPost}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(post)
-      })
-        .then(response => response.json())
-        .then(data => {
-          setPosts([...posts, data]);
-          setTitle('');
-          setContent('');
-        });
-    }
-  };
-
-  const handleEdit = (post) => {
-    setTitle(post.title);
-    setContent(post.content);
-    setEditId(post.id);
-  };
-
-  const handleDelete = (id) => {
-    fetch(`${process.env.REACT_APP_API_URL}/${endPointPost}/${id}`, {
-      method: 'DELETE'
-    })
-      .then(() => {
-        setPosts(posts.filter(post => post.id !== id));
-      });
-  };
   return (
-    <div>
-      <h1>Posts</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title:</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Content:</label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          ></textarea>
-        </div>
-        <button type="submit">{editId ? 'Update Post' : 'Add Post'}</button>
-      </form>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-            <button onClick={() => handleEdit(post)}>Edit</button>
-            <button onClick={() => handleDelete(post.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <header className="App-header">
+        <p>{message}</p>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
